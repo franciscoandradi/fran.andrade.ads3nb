ae61f4dbee93dda42ce12f1032937b0d33f0b861
franciscoandradi <166152118+franciscoandradi@users.noreply.github.com>
2025-04-02 00:24:04 +0000

    desafio 2 finalizado

--- aula5/src/lib.rs
+++ aula5/src/lib.rs
@@ -4,7 +4,7 @@
 #[cfg(test)]
 mod bst_tests {
     // Importe sua implementação de BST aqui
-    // use crate::BST;
+     use crate::BST;
     
     #[test]
     fn test_bst_new_and_empty() {
@@ -22,6 +22,10 @@ mod bst_tests {
         bst.insert(10);
         bst.insert(5);
         bst.insert(15);
+
+        println!("10: {}", bst.search(10));
+        println!("5: {}", bst.search(5));
+        println!("15: {}", bst.search(15));
         
         // Verificar se os valores inseridos estão na árvore
         assert!(bst.search(10));
@@ -35,19 +39,21 @@ mod bst_tests {
         assert!(!bst.is_empty());
     }
 }
-
+#[derive(Debug)]
 struct Node {
     value: i32,
     left: Option<Box<Node>>,
     right: Option<Box<Node>>
 }
 
+//usei do arquivo main da pasta da aula que fizemos em sala
 // Esqueleto para implementação da BST pelos alunos
 #[derive(Debug)]
 struct BST {
     root: Option<Box<Node>>
 }
 
+//usei tambem do arquivo main da pasta da aula que fizemos em sala
 impl BST {
     // Criar uma nova árvore vazia
     fn new() -> Self {
@@ -55,12 +61,14 @@ impl BST {
 
     }
     
+    //aqui e onde a funcao "is empty" vai ser criada, ela sera um bool pois e assim que vamos saber se esta vazia ou nao, com true ou false,
+    //entao se arvore estiver vazia ela retorna true e caso contrario ela retorna false
     // Verificar se a árvore está vazia
     fn is_empty(&self) -> bool {
-        // Implementar
-        unimplemented!()
+        self.root.is_none()
     }
     
+    //usei tambem do arquivo main da pasta da aula que fizemos em sala
     // Inserir um valor na árvore
     fn insert(&mut self, value: i32) {
         let mut current: &mut Option<Box<Node>> = &mut self.root;
@@ -89,6 +97,7 @@ impl BST {
         }
     }
     
+    //usei tambem do arquivo main da pasta da aula que fizemos em sala
     // Buscar um valor na árvore
     fn search(&self, value: i32) -> bool {
         let mut current = &self.root;
