0db8bfda22a7fe8428b661eba595bef99502d969
franciscoandradi <166152118+franciscoandradi@users.noreply.github.com>
2025-05-26 23:19:22 +0000

    Atualizado para prova

--- prova2/src/main.rs
+++ prova2/src/main.rs
@@ -1,65 +1,12 @@
-
-struct VetorOrdenavel {
-     dados: Vec<i32>,
-}
-
-impl VetorOrdenavel {
-    fn novo() -> Self {
-        let mut dados = [34, 7, 23, 32, 5, 62, 31, 12, 43, 3];
-        VetorOrdenavel {  dados: dados.to_vec() }
-    }
-
-    fn exibir(&self) {
-        println!("{:?}", self.dados);
-    }
-
-    fn ordenar(&mut self) {
-        Self::quicksort(&mut self.dados);
-    }
-
-    fn quicksort(vetor: &mut [i32]) {
-        let len = vetor.len();
-        if len <= 1 {
-            return;
-        }
-
-        let pivot_index = len / 2;
-        let pivot = vetor[pivot_index];
-
-        let mut left = 0;
-        let mut right = len - 1;
-
-        while left <= right {
-            while vetor[left] < pivot {
-                left += 1;
-            }
-            while vetor[right] > pivot {
-                right = right.saturating_sub(1);
-            }
-            if left <= right {
-                vetor.swap(left, right);
-                left += 1;
-                right = right.saturating_sub(1);
-            }
-        }
-
-        if right > 0 {
-            Self::quicksort(&mut vetor[0..=right]);
-        }
-        if left < len {
-            Self::quicksort(&mut vetor[left..]);
-        }
-    }
-}
+use std::collections::VecDeque; 
 
 fn main() {
-    let mut vetor = VetorOrdenavel::novo();
+    let mut vetor = vec![34, 7, 23, 32, 5, 62, 31, 12, 43, 3];
 
-    println!("Vetor original:");
-    vetor.exibir();
+    println!("Vetor original: {:?}", vetor);
 
-    vetor.ordenar();
+    // Ordenando o vetor com sort()
+    vetor.sort();
 
-    println!("Vetor ordenado:");
-    vetor.exibir();
+    println!("Vetor ordenado: {:?}", vetor);
 }
