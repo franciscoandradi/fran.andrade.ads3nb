1327a0a176a3ddbe742c7df33fc6276e75264552
franciscoandradi <166152118+franciscoandradi@users.noreply.github.com>
2025-05-26 22:57:45 +0000

    Atualizado para prova

--- prova2/src/main.rs
+++ prova2/src/main.rs
@@ -1,18 +1,3 @@
-//NIVEL 1
-
-fn main() {
-    
-    let mut vetor = [34, 7, 23, 32, 5, 62, 31, 12, 43, 3];
-
-    println!("Vetor original: {:?}", vetor);
-
-    // Ordenando o vetor com sort()
-    vetor.sort();
-
-    println!("Vetor ordenado: {:?}", vetor);
-}
-
-//NIVEL 2
 
 struct VetorOrdenavel {
      dados: Vec<i32>,
